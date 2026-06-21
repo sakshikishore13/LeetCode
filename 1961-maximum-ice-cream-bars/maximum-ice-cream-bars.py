0997b994091a758e1073209600317359ea5ae30a
sakshikishore13 <147229700+sakshikishore13@users.noreply.github.com>
2026-06-21 20:22:47 +0530

    Time: 112 ms (85.39%) | Memory: 20.9 MB (56.18%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxIceCream(self, costs, coins):
+        """
+        :type costs: List[int]
+        :type coins: int
+        :rtype: int
+        """
+        costs.sort()
+
+        count = 0
+
+        for cost in costs:
+
+            if coins >= cost:
+                coins -= cost
+                count += 1
+            else:
+                break
+
+        return count
