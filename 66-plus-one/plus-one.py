66b1c4448075223d0a65f774d68fb646a7c03905
sakshikishore13 <147229700+sakshikishore13@users.noreply.github.com>
2026-06-27 19:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.05%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        # If all digits are 9
+        return [1] + [0] * len(digits)
